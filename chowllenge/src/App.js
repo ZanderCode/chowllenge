acd94ffe722d9acebe5b57b66e9daea2e516195b
Zander <alexlamarche90@gmail.com>
2022-03-05 17:39:05 -0500

    +

--- chowllenge/src/App.js
+++ chowllenge/src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import DocumentMeta from 'react-document-meta';
+
+
+const meta = {
+  title: 'Title',
+  meta: {
+      charset: 'utf-8',
+      name: {
+          name:"mobile-web-app-capable", 
+          content:"yes",
+          keywords: 'react,meta,document,html,tags'
+      }
+  }
+}
 
 function App() {
+
   return (
     <div className="App">
-      Zander Rules
+      Yoe
+      <DocumentMeta {...meta} />
+      <div style={{"background-color":"blue","color":"white"}}>Zander Code</div>
     </div>
   );
 }
